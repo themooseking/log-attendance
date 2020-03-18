3f94afed9cbe480f8c03eef3430db2193cdae0ba
Jamil <Jamil@Jamil-PC>
2020-03-18 12:19:48 +0100

    Calculate Attendance 0.1.0

---
+++ ALS/src/logic/CalculateAttendance.java
@@ -0,0 +1,14 @@
+package logic;
+
+import java.sql.Date;
+
+public class CalculateAttendance {
+	private Date startDate;
+	private Date endDate;
+	private DB_Controller controller;
+	
+	private void calcAttendancePerc() {
+		controller.getAbsenceList;
+		
+	}
+}
---
+++ ALS/src/logic/DB_Controller.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class DB_Controller {
+
+	public Object getAbsenceList;
+
+}
