8c5403da6ed237e4b88e8ab5de79e63ec90ea2e6
Dean <1thagok1@gmail.com>
2020-03-18 14:50:00 +0100

    TimeTable

--- ALS/src/entities/Timetable.java
+++ ALS/src/entities/Timetable.java
@@ -1,11 +1,10 @@
 package entities;
 
 public class Timetable {
-	
 	private String plannedDay;
 	private Course course;
 	
-	Timetable(String plannedDay, Course course){
+	public Timetable(String plannedDay, Course course){
 		this.plannedDay = plannedDay;
 		this.course = course;
 	}
--- ALS/src/logic/DB_Controller.java
+++ ALS/src/logic/DB_Controller.java
@@ -9,6 +9,7 @@ public class DB_Controller {
 	private DB_Absence absenceDB = new DB_Absence(dataLayer.getConnection());
 	private DB_Course courseDB = new DB_Course(dataLayer.getConnection());
 	private DB_Student studentDB = new DB_Student(dataLayer.getConnection());
+	private DB_TimeTable timeTableDB = new DB_TimeTable(dataLayer.getConnection());
 	
 	/***********************************
 	 * READ
@@ -27,5 +28,11 @@ public class DB_Controller {
 	
 	public ArrayList<Student> getStudentList(){
 		return studentDB.getStudents();
+	}
+	
+	public ArrayList<Timetable> getTimeTableList(){
+		ArrayList<Course> courseList = getCourseList();
+		
+		return timeTableDB.getTimeTable(courseList);
 	}
 }
