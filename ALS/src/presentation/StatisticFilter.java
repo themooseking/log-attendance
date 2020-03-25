e0e8ef9ee0b52bb1f49b03edd68d7297fcc4b706
Jamil <Jamil@Jamil-PC>
2020-03-25 12:14:54 +0100

    HotFix

--- ALS/src/presentation/StatisticFilter.java
+++ ALS/src/presentation/StatisticFilter.java
@@ -4,12 +4,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import entities.Course;
-import entities.Student;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
