8c80ba059f5e911985f3c18832966128a906e945
themooseking <55086952+themooseking@users.noreply.github.com>
2020-04-01 11:03:45 +0200

    registerabsence

--- ALS/src/logic/RegisterAbsence.java
+++ ALS/src/logic/RegisterAbsence.java
@@ -1,19 +1,39 @@
 package logic;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+import entities.Absence;
 import entities.Course;
 import entities.Student;
 
 public class RegisterAbsence {
 	private ArrayList<Student> selectedStudentList;
 	private Course selectedCourse;
+	private LocalDate today = LocalDate.now();
+	private DB_Controller controller = new DB_Controller();
+	
 	
 	public RegisterAbsence(ArrayList<Student> selectedStudentList, Course selectedCourse) {
 		this.selectedStudentList = selectedStudentList;
 		this.selectedCourse = selectedCourse;
+		logAbsence();
+		//controller.createAbsence(studentAbsenceList());
 	}
 
 
+	private ArrayList<Absence> studentAbsenceList() {
+		ArrayList<Absence> studentAbsenceList = new ArrayList<Absence>();
+		
+		for (int i = 0; i < selectedStudentList.size(); i++) {
+			studentAbsenceList.add(new Absence(today, selectedStudentList.get(i), selectedCourse));
+		}
+		
+		return studentAbsenceList;
+	}
+	
+	public void logAbsence() {
+		controller.createAbsence(studentAbsenceList());
+	}
 
 }
