9331c2acbd92674171879feb5c0235059411fcc4
Jamil <Jamil@Jamil-PC>
2020-04-02 13:47:33 +0200

    .

--- ALS/src/presentation/Main.java
+++ ALS/src/presentation/Main.java
@@ -8,16 +8,28 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		CourseStatistics stats = new CourseStatistics(primaryStage);
-		stats.courseStatisticsUI();
-	}
 
 //	@Override
 //	public void start(Stage primaryStage) throws Exception {
-//		LoginScreen login = new LoginScreen(primaryStage); 
-//		login.loginUI();
+//		LogAttendance la = new LogAttendance(primaryStage); 
+//		la.logAttendanceUI();
 //	}
+
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		StatisticFilter sf = new StatisticFilter(primaryStage);
+//		sf.filterUI();
+//	}
+
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		CourseStatistics stats = new CourseStatistics(primaryStage);
+//		stats.courseStatisticsUI();
+//	} 
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		LoginScreen login = new LoginScreen(primaryStage); 
+		login.loginUI();
+	}
 }
