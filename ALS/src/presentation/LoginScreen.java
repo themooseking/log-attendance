a2b0aac45aaddaf2c0e1b1ea439058890d486bf4
emilo <emilo@DESKTOP-IME9A9E>
2020-04-08 11:46:13 +0200

    NameArray

--- ALS/src/presentation/LoginScreen.java
+++ ALS/src/presentation/LoginScreen.java
@@ -1,5 +1,7 @@
 package presentation;
 
+import java.util.ArrayList;
+
 import entities.Educator;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
@@ -20,11 +22,12 @@ public class LoginScreen {
 	private Stage primaryStage;
 	private VBoxWithStyle vbox;
 	private DB_Controller controller = new DB_Controller();
+	private ArrayList<Educator> educatorList;
 	private Educator educator = new Educator(1, "Hans Iversen");
 
 	public LoginScreen(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		
+		this.educatorList = controller.getEducatorList();
 	}
 
 	public void loginUI() {
@@ -35,11 +38,15 @@ public class LoginScreen {
 		sceneSetup(scene);
 	}
 	
-	private ComboBox slcEducator() {
-		String edu[] = { "Hans Iversen", "Flemming Kock Jensen"}; 
+	private ComboBox<String> slcEducator() {
+		String educatorNameArray[] = new String[educatorList.size()];
 		
-		ComboBox slcEducator = new ComboBox(FXCollections.observableArrayList(edu));
-
+		for (int i = 0; i < educatorList.size(); i++) {
+			educatorNameArray[i] = educatorList.get(i).getEducatorName();
+		}
+		
+		ComboBox<String> slcEducator = new ComboBox<String>(FXCollections.observableArrayList(educatorNameArray));
+		
 		slcEducator.setMinSize(150, 50);
 		
 		return slcEducator;
